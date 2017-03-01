7967aa67f4e7ba22a5de7b16fc74179985c611fb
Joni Saksholm <joni.saksholm@bertschi.fi>
2017-03-01 14:58:59 +0000

    redux things

---
+++ riff-laspapeleras/src/Wrapper.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import BinList from './bins/BinList';
+import Map from './map/Map';
+
+import {connect} from 'react-redux';
+
+
+const Wrapper = () => {
+  return (
+      <div className="wrapper">
+        <BinList></BinList>
+        <Map></Map>
+      </div>)
+};
+
+const mapStateToProps = (state) => {
+  return {
+    bins: state.bins,
+  }
+};
+
+export default connect(mapStateToProps)(Wrapper);
