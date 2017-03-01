c59b74cb9cd1a331e3a0d62e0b3d612e034d1fae
Joni Saksholm <joni.saksholm@bertschi.fi>
2017-03-01 14:29:22 +0000

    stuff

--- riff-laspapeleras/src/App.js
+++ riff-laspapeleras/src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import BinList from './bins/BinList';
+import Map from './map/Map';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="wrapper">
+        <BinList></BinList>
+        <Map></Map>
       </div>
     );
   }
